b8bc83b80929a0358ec9e41f9f9aab8eafeae5f2
baph1017 <32640618+baph1017@users.noreply.github.com>
2018-01-12 20:04:13 +0100

    Update update-stammdaten.component.ts
     this.kunde.updateStammdaten verbessert

--- src/kunde/update/update-stammdaten.component.ts
+++ src/kunde/update/update-stammdaten.component.ts
@@ -65,10 +65,9 @@ export default class UpdateStammdatenComponent implements OnInit {
         this.geschlecht = new FormControl(this.kunde.geschlecht)
         this.kategorie = new FormControl(this.kunde.kategorie)
         // this.geburtsdatum = new Control(this.kunde.geburtsdatum.toISOString())
-
         this.form = this.formBuilder.group({
             // siehe formControlName innerhalb von @Component({template: ...})
-            namenach: this.nachname,
+            nachname: this.nachname,
             familienstand: this.familienstand,
             geschlecht: this.geschlecht,
             kategorie: this.kategorie,
@@ -96,13 +95,11 @@ export default class UpdateStammdatenComponent implements OnInit {
 
         // kategorie, kategorie und rabatt koennen im Formular nicht geaendert werden
         this.kunde.updateStammdaten(
-            this.nachname.value, this.familienstand.value, this.geschlecht.value,
-            this.kunde.kategorie, this.kunde.newsletter, this.kunde.interessen,
-            this.kunde.email, this.kunde.adresse, this.kunde.homepage,
+            this.nachname.value, this.geschlecht.value, this.familienstand.value, this.kategorie.value,
+            this.kunde.newsletter, this.kunde.email, this.kunde.geburtsdatum,
+            this.kunde.user, this.kunde.adresse, this.kunde.homepage,
             this.kunde.umsatz)
         console.log('kunde=', this.kunde)
-
-        
 
         const successFn = () => {
             console.log(`UpdateStammdaten: successFn: path: ${HOME_PATH}`)
