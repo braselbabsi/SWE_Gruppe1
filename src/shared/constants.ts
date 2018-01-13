5b4972a421a105705b1ac2441313cf2f45e8277d
buda1021 <32611105+buda1021@users.noreply.github.com>
2018-01-13 15:23:36 +0100

    Update constants.ts

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -28,7 +28,7 @@ export const BASE_URI = `${SCHEME}://${SERVERNAME}:${PORT}${BASE_PATH}`
 /**
  * Pfad f&uuml;r den Zugriff auf B&uuml;cher, ausgehend von der Basis-URI.
  */
-export const PATH_KUNDE = `buch`
+export const PATH_KUNDE = `kunde`
 
 export const TIMEZONE_OFFSET_MS =
     new Date().getTimezoneOffset() * 60 * 1000
