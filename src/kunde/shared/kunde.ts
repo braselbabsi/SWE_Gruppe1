b6d0c1b4db4b0794178dac3e0fb41321983bd97b
buda1021 <32611105+buda1021@users.noreply.github.com>
2018-01-15 18:15:33 +0100

    Update kunde.ts

--- src/kunde/shared/kunde.ts
+++ src/kunde/shared/kunde.ts
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import 'andresse.ts'
-import 'umsatz.ts'
+import '/adresse'
+import '/umsatz'
 
 import * as _ from 'lodash'
 import * as moment from 'moment'
